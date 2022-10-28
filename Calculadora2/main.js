21892e4557f17cdf28140b89f77c5575560ac74b
instak1ll <mamifero3004@gmail.com>
2022-10-28 22:29:44 +0200

    undefined

--- Calculadora2/main.js
+++ Calculadora2/main.js
@@ -1,9 +1,39 @@
-<script>
-let x, y, z;
-x = 5;
-y = 6;
-z = x + y;
+/*let edad=prompt("Introduce la edad");
+if(Number(edad)==edad){
+    if(edad>=18){alert("puedes conducir");}
+    else if(edad<18){alert("no puedes conducir");}
+}else{alert("Introduce un valor numerico");}*/
 
-document.getElementById("demo").innerHTML =
-"The value of z is " + z + ".";  
-</script>
+do{
+    let nota=prompt("Introduce tu nota");
+    if(Number(nota)==nota){
+        if(nota<0&&nota>=10){
+            if(nota<3){
+                alert("Muy deficiente");
+            }
+            else if(nota<5){
+                alert("Insuficiente");
+            }
+            else if(nota<7){
+                alert("Suficiente");
+            }
+            else if(nota<8){
+                alert("Bien");
+            }
+            else if(nota<9){
+                alert("Notable");
+            }
+            else if(nota>=9){
+                alert("Sobresaliente");
+            }
+        }
+        else{
+            alert("Introduce un valor valido");
+        }
+    }
+    else{
+        if(nota!=undefined){
+            alert("Introduce un numero valido");
+        }
+    }
+}while(nota!=undefined);
